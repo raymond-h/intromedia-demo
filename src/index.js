1f47b334ce22f10adf947ec256cdf9d728ab4018
Raymond Hammarling <raykay.rh@gmail.com>
2016-05-13 13:03:57 +0100

    Setup server-side React template rendering

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// The entry point of all to come...
+import path from 'path';
+
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+const app = express();
+
+app.set('env', 'development');
+app.set('views', './lib/views');
+app.set('view engine', 'js');
+
+app.engine('js', function(filePath, opts, cb) {
+    const Component = require(filePath);
+
+    cb(null, ReactDOMServer.renderToString(<Component {...opts} />));
+});
+
+app.use(express.static( path.join(__dirname, '../public') ));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080);
---
+++ src/views/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return <html>
+            <head>
+                <title>Cool!!</title>
+            </head>
+            <body>
+                Very cool!
+                <script src="index.js"></script>
+            </body>
+        </html>;
+    }
+}
+
+module.exports = Index;
